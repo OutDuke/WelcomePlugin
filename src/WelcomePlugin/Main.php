5578a9359ecbd9408628e8d042d47f0a5370a385
unknown <naahardas04@gmail.com>
2025-06-03 14:18:14 +0530

    Intial commit

---
+++ src/WelcomePlugin/Main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WelcomePlugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getLogger()->info("WelcomePlugin has been enabled!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $player->sendMessage("§aWelcome to the server, " . $player->getName() . "!");
+    }
+}
